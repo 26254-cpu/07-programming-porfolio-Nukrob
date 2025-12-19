1b762b5ee1e47dc689f4bae29fb1a5af78a14678
26254Nukrob <26254@bbw.ac.th>
2025-12-19 15:28:07 +0700

    สร้างโดยใช้ Colab

---
+++ mid-(07)-Nukrob.py
@@ -0,0 +1,60 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPGTwR9yZi4opoHY2V0X8d4",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/26254-cpu/07-programming-porfolio-Nukrob/blob/main/mid-(07)-Nukrob.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "#รับข้อมูล Input\n",
+        "num=int(input(\"กรุณาระบุค่าคงที่ ที่อยู่ในช่วง จำนวนเต็มลบ,ศูนย์ และ จำนวนเต็มบวก\"))\n",
+        "#ประมวผล (Process) และ แสดงผล (Output)\n",
+        "if num > 0:\n",
+        "  print('มากกว่า 0')\n",
+        "else:\n",
+        "  print(\"น้อยกว่าหรือเท่ากับ 0\")"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "22TO9EaQrj5P",
+        "outputId": "26d23d39-ab8c-4866-8382-eaae381e407a"
+      },
+      "execution_count": 18,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "กรุณาระบุค่าคงที่ ที่อยู่ในช่วง จำนวนเต็มลบ,ศูนย์ และ จำนวนเต็มบวก-56\n",
+            "น้อยกว่าหรือเท่ากับ 0\n"
+          ]
+        }
+      ]
+    }
+  ]
+}
